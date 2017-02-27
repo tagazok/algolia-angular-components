f232b7666b6fce169dbcc75baed43f04c4824c58
Olivier <leplus.olivier@gmail.com>
2017-02-27 17:20:36 +0100

    feat(results): Remove css dependencies

--- src/lib/algolia-results/algolia-results.component.ts
+++ src/lib/algolia-results/algolia-results.component.ts
@@ -4,54 +4,12 @@ import { AlgoliaService } from '../algolia.service';
 @Component({
   selector: 'app-algolia-results',
   template: `
-  <div class="flex-row flex-space-around flex-center flex-wrap">
-    <div class="my-item flex-column col-width" *ngFor="let result of as.content.hits; let i = index">
-      <template [ngTemplateOutlet]="itemTemplate" [ngOutletContext]="{item: result, index: i}"></template>
-    </div>
-  </div>
+    <template  *ngFor="let result of as.content.hits; let i = index" [ngTemplateOutlet]="itemTemplate" [ngOutletContext]="{item: result, index: i}"></template>
   `,
   styles: [`
   .my-item {
     padding: .5%;
     width: 100%;
-  }
-
-    
-  @media (min-width: 1280px) {
-    .col-width { max-width: 25%; }
-  }
-  @media (min-width: 960px) and (max-width: 1279px) {
-    .col-width { max-width: 33%; }
-  }
-  @media (min-width: 600px) and (max-width: 959px) {
-    .col-width { max-width: 50%; }
-  }
-
-  .flex-row {
-    flex-flow: row wrap;
-    background-color: rgb(228, 228, 228);
-    box-sizing: border-box;
-    max-height: 100%;
-    display: flex;
-  }
-
-  .flex-column {
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .flex-wrap {
-    flex-wrap: wrap;
-  }
-
-  .flex-space-around {
-    justify-content: space-around;
-  }
-
-  .flex-center {
-    align-items: center;
-    align-content: center;
   }
 `]
 })
